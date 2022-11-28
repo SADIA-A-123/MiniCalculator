ca011564c205a843aa2802ca905852260bae9681
Sadia <bsem-f20-093@superior.edu.pk>
2022-11-28 11:21:41 +0500

    Commit

---
+++ app/src/main/java/com/example/minicalculator/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.minicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    private TextView TextViewResult, TextViewHistory;
+    private
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
